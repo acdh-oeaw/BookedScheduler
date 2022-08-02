2279d821cdd2e0c35a7963ce9f289a95d3126b48
Pancic <Dalibor.Pancic@oeaw.ac.at>
2022-08-02 14:16:27 +0200

    add more variables

--- initandrun.sh
+++ initandrun.sh
@@ -15,6 +15,14 @@ echo '$conf["settings"]["database"]["name"] = ' "'$DB_DATABASE';" >>config/confi
 echo '$conf["settings"]["install.password"] = ' "'$INSTALL_PASSWORD';" >>config/config.php; \
 echo '$conf["settings"]["sript.url"] = ' "'$SCRIPT_URL';" >>config/config.php; \
 echo '$conf["settings"]["admin.email"] = ' "'$ADMIN_EMAIL';" >>config/config.php; \
+echo '$conf["settings"]["email"]["default.from.address"] = ' "'$DEFAULT_FROM_EMAIL';" >>config/config.php; \
+echo '$conf["settings"]["email"]["default.from.name"] = ' "'$DEFAULT_FROM_NAME';" >>config/config.php; \
+echo '$conf["settings"]["image.upload.directory"] = ' "'$IMAGE_UPLOAD_DIR';" >>config/config.php; \
+echo '$conf["settings"]["default.language"] = ' "'$DEFAULT_LANG';" >>config/config.php; \
+echo '$conf["settings"]["admin.email.name"] = ' "'$ADMIN_EMAIL_NAME';" >>config/config.php; \
+echo '$conf["settings"]["app.title"] = ' "'$APP_TITLE';" >>config/config.php; \
+echo '$conf["settings"]["default.timezone"] = ' "'$DEF_TZ';" >>config/config.php; \
+echo '$conf["settings"]["allow.self.registration"] = ' "'$ALLOW_SELF_REGISTRATION';" >>config/config.php; \
 echo '$conf["settings"]["phpmailer"]["smtp.host"] = ' "'$SMTP_HOST';" >>config/config.php; 
 
 # Start Apache
