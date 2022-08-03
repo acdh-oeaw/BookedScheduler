cddfb6fc0917346c88ec06908f45e114480bb2cb
Pancic <Dalibor.Pancic@oeaw.ac.at>
2022-08-03 12:02:35 +0200

    add LDAP config

--- initandrun.sh
+++ initandrun.sh
@@ -6,6 +6,8 @@ export PATH="/app/.heroku/php/bin:${PATH}"
 # Add custom commands
 cd /app
 rm -f config/config.php
+rm -f plugins/Authentication/Ldap/Ldap.config.php 
+cp -r plugins/Authentication/Ldap/Ldap.config.dist.php plugins/Authentication/Ldap/Ldap.config.php
 if [ "$LB_ENV" = "production" ]; then cp config/config.dist.php config/config.php; fi; \
 if [ "$LB_ENV" = "dev" ]; then cp config/config.devel.php config/config.php; fi; \
 echo '$conf["settings"]["database"]["user"] = ' "'$DB_USER';" >>config/config.php; \
@@ -24,6 +26,13 @@ echo '$conf["settings"]["app.title"] = ' "'$APP_TITLE';" >>config/config.php; \
 echo '$conf["settings"]["default.timezone"] = ' "'$DEF_TZ';" >>config/config.php; \
 echo '$conf["settings"]["allow.self.registration"] = ' "'$ALLOW_SELF_REGISTRATION';" >>config/config.php; \
 echo '$conf["settings"]["plugins"]["Authentication"] =  ' "'$AUTH';" >>config/config.php; \
+echo '$conf["settings"]["host"] =  ' "'$LDAP_HOST';" >>plugins/Authentication/Ldap/Ldap.config.php; \
+echo '$conf["settings"]["port"] =  ' "'$LDAP_PORT';" >>plugins/Authentication/Ldap/Ldap.config.php; \
+echo '$conf["settings"]["binddn"] =  ' "'$LDAP_BIND_DN';" >>plugins/Authentication/Ldap/Ldap.config.php; \
+echo '$conf["settings"]["bindpw"] =  ' "'$LDAP_BIND_PWD';" >>plugins/Authentication/Ldap/Ldap.config.php; \
+echo '$conf["settings"]["basedn"] =  ' "'$LDAP_BASE_DN';" >>plugins/Authentication/Ldap/Ldap.config.php; \
+echo '$conf["settings"]["attribute.mapping"] =  ' "'$LDAP_ATTR_MAP';" >>plugins/Authentication/Ldap/Ldap.config.php; \
+echo '$conf["settings"]["user.id.attribute"] =  ' "'$LDAP_USERID_ATTR';" >>plugins/Authentication/Ldap/Ldap.config.php; \
 echo '$conf["settings"]["phpmailer"]["smtp.host"] = ' "'$SMTP_HOST';" >>config/config.php; 
 
 # Start Apache
